5a0f3f9e38bced1f5220975ea2cb4aef61ce8708
Andrey <sarkis.06@mail.ru>
2025-03-02 23:48:10 +0500

    Итоговое решение

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,14 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component ("needle")
 public class Needle7 {
+
+    public Needle7() {}
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " ;
     }
 }
